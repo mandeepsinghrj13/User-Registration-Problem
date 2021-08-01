266436510de95eb4dc6f56604061a4bfab0683b7
mandeep <singh.mr.1996@gmail.com>
2021-08-01 20:18:38 +0530

    Added: Last name start with cap and has minimum 3 char

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,8 +1,6 @@
 package com.bridgelabz;
 import java.util.Scanner;
-//import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 /**
  * @author mandeep
  */
@@ -15,5 +13,14 @@ public class UserRegistration {
         Scanner scanner = new Scanner(System.in);
         String firstName = scanner.next();
         System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
+    }
+    /**
+     * Last name must start with Capital letter and has minimum 3 length
+     */
+    public static void lastNameValidate() {
+        System.out.print("Enter last Name : ");
+        Scanner scanner = new Scanner(System.in);
+        String lastName = scanner.next();
+        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", lastName));
     }
 }
--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -1,11 +1,9 @@
 package com.bridgelabz;
-import java.util.Scanner;
 public class UserRegistrationMain {
-    Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("------Welcome to User Registration-----");
-       // Scanner scanner = new Scanner(System.in);
         UserRegistration userRegistration = new UserRegistration();
         userRegistration.firstNameValidate();
+        userRegistration.lastNameValidate();
     }
 }
