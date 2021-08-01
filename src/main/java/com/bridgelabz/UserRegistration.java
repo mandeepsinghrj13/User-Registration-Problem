7f9a9a3b90532ec5e564ab50d0a40903cd93ddeb
mandeep <singh.mr.1996@gmail.com>
2021-08-01 20:44:26 +0530

    Added: Enter a vaild email

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 /**
- * @author mandeep
+ * @author Mandeep
  */
 public class UserRegistration {
     /**
@@ -22,5 +22,16 @@ public class UserRegistration {
         Scanner scanner = new Scanner(System.in);
         String lastName = scanner.next();
         System.out.println(Pattern.matches("[A-Z][a-z]{2,}", lastName));
+    }
+
+    /**
+     * Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+     * E.g. abc.xyz@bl.co.in
+     */
+    public static void emailValidate() {
+        System.out.print("Enter Email : ");
+        Scanner scanner = new Scanner(System.in);
+        String email = scanner.next();
+        System.out.println(Pattern.matches("^[a-zA-Z]+([.][a-zA-Z]+)*@[a-zA-Z]+[.][a-zA-Z]{2,3}+([.][a-zA-Z]+)*$", email));
     }
 }
--- src/main/java/com/bridgelabz/UserRegistrationMain.java
+++ src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -1,9 +1,23 @@
 package com.bridgelabz;
 public class UserRegistrationMain {
+    /**
+     * @author Mandeep
+     * Creating a main method
+     */
     public static void main(String[] args) {
+        /**
+         * Welcome message
+         */
         System.out.println("------Welcome to User Registration-----");
+        /**
+         * Creating A Object
+         */
         UserRegistration userRegistration = new UserRegistration();
+        /**
+         * Calling method throw Object reference
+         */
         userRegistration.firstNameValidate();
         userRegistration.lastNameValidate();
+        userRegistration.emailValidate();
     }
 }
