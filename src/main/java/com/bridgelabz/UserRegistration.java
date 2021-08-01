40ff59aa9e21d410b85965a343d3adbaebb4ba44
mandeep <singh.mr.1996@gmail.com>
2021-08-01 17:41:39 +0530

    Added: First name starts with Cap and has minimum 3 characters

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,4 +1,19 @@
 package com.bridgelabz;
+import java.util.Scanner;
+//import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * @author mandeep
+ */
 public class UserRegistration {
+    /**
+     * First name must start with Capital letter and has minimum 3 length
+     */
+    public static void firstNameValidate() {
+        System.out.print("Enter first Name : ");
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.next();
+        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
+    }
 }
